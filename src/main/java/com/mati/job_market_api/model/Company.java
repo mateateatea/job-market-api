28b7793f92864178fba58de4ede0e7f9fa62185c
mateateatea <213237051+mateateatea@users.noreply.github.com>
2026-08-11 14:01:47 +0200

    Add Company entity and configure database connection

---
+++ src/main/java/com/mati/job_market_api/model/Company.java
@@ -0,0 +1,33 @@
+package com.mati.job_market_api.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "companies")
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "company_id")
+    private Integer companyId;
+
+    @Column(name = "name")
+    private String name;
+
+    // Getters and setters
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
