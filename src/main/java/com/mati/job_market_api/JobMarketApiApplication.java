94ae1266983ba3740310b02d3886bcbd833f3249
mateateatea <213237051+mateateatea@users.noreply.github.com>
2026-08-11 12:45:02 +0200

    Initial Spring Boot project skeleton

---
+++ src/main/java/com/mati/job_market_api/JobMarketApiApplication.java
@@ -0,0 +1,13 @@
+package com.mati.job_market_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobMarketApiApplication.class, args);
+	}
+
+}
