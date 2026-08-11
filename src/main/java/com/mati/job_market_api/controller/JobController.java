a5968314aa0cae7e77b22c54de3906369d3384a2
mateateatea <213237051+mateateatea@users.noreply.github.com>
2026-08-11 23:41:34 +0200

    Implement full CRUD for Job entity with Company relationshiop

--- src/main/java/com/mati/job_market_api/controller/JobController.java
+++ src/main/java/com/mati/job_market_api/controller/JobController.java
@@ -27,4 +27,31 @@ public class JobController {
     public Optional<Job> getJobById(@PathVariable Integer id) {
         return jobRepository.findById(id);
     }
+
+    @PostMapping
+    public Job createJob(@RequestBody Job job) {
+        return jobRepository.save(job);
+    }
+
+    @PutMapping("/{id}")
+    public Job updateJob(@PathVariable Integer id, @RequestBody Job jobDetails) {
+        Job job = jobRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Job not found with id " + id));
+
+        job.setTitle(jobDetails.getTitle());
+        job.setCity(jobDetails.getCity());
+        job.setWorkModel(jobDetails.getWorkModel());
+        job.setSeniority(jobDetails.getSeniority());
+        job.setSource(jobDetails.getSource());
+        job.setDatePosted(jobDetails.getDatePosted());
+        job.setLink(jobDetails.getLink());
+        job.setCompany(jobDetails.getCompany());
+
+        return jobRepository.save(job);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteJob(@PathVariable Integer id) {
+        jobRepository.deleteById(id);
+    }
 }
