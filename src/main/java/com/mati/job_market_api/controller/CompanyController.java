fb7c898b495f3646857854dd73368a44d61315bc
mateateatea <213237051+mateateatea@users.noreply.github.com>
2026-08-12 13:02:29 +0200

    Add Company Controller

---
+++ src/main/java/com/mati/job_market_api/controller/CompanyController.java
@@ -0,0 +1,50 @@
+package com.mati.job_market_api.controller;
+
+import com.mati.job_market_api.model.Company;
+import com.mati.job_market_api.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/companies")
+public class CompanyController {
+
+    private final CompanyRepository companyRepository;
+
+    public CompanyController(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    @GetMapping
+    public List<Company> getAllCompanies() {
+        return companyRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Optional<Company> getCompanyById(@PathVariable Integer id) {
+        return companyRepository.findById(id);
+    }
+
+    @PostMapping
+    public Company createCompany(@RequestBody Company company) {
+        return companyRepository.save(company);
+    }
+
+    @PutMapping("/{id}")
+    public Company updateCompany(@PathVariable Integer id, @RequestBody Company companyDetails) {
+        Company company = companyRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Company not found with id " + id));
+
+        company.setName(companyDetails.getName());
+
+        return companyRepository.save(company);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCompany(@PathVariable Integer id){
+        companyRepository.deleteById(id);
+    }
+}
