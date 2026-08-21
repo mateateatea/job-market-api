1df7c9176928af26d603352885ab47d8ea1c4a2a
mateateatea <213237051+mateateatea@users.noreply.github.com>
2026-08-21 18:27:23 +0200

    feat: implement SkillList component with data fetching

--- frontend/src/app/components/skill-list/skill-list.ts
+++ frontend/src/app/components/skill-list/skill-list.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import {SkillService} from '../../services/skill.service';
+import {Skill} from  '../../models/skill.model';
 
 @Component({
   selector: 'app-skill-list',
@@ -6,4 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './skill-list.html',
   styleUrl: './skill-list.css',
 })
-export class SkillList {}
+export class SkillList implements OnInit {
+  private SkillService = inject(SkillService);
+
+  skills = signal<Skill[]>([]);
+
+  ngOnInit() {
+    this.SkillService.getAllSkills().subscribe({
+      next: data => {
+        this.skills.set(data);
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
+}
